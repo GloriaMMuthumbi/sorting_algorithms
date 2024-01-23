3166fcbf0c1c7a95530b5c39087ec8054394268d
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-01-23 03:47:06 -0800

    0. Bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,49 @@
+#include "sort.h"
+/**
+ * bubble_sort - sorts an array of integers in ascending
+ * order using Bubble sort algorithm. prints the array
+ * after every swap of elements
+ * @array: array of integers
+ * @size: size of te array
+ * Return: returns nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	bool sorted = false;
+	size_t i, len;
+
+	len = size;
+	if (array == NULL || size < 2)
+		return;
+
+	while (sorted == false)
+	{
+		sorted = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_elems(array + i, array + i + 1 );
+				print_array(array, size);
+				sorted = false;
+			}
+		}
+		len--;
+	}
+
+}
+
+/**
+ * swap_elems - swaps two adjacent elements of the array
+ * @elem1: first element
+ * @elem2: second element
+ * Return: returns nothing
+ */
+void swap_elems(int *elem1, int *elem2)
+{
+	int temp;
+
+	temp = *elem1;
+	*elem1 = *elem2;
+	*elem2 = temp;
+}
---
+++ sort.h
@@ -0,0 +1,38 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/**
+ * enum bool = Enumeration of boolean values
+ */
+typedef enum bool
+{
+	false = 0,
+	true
+} bool;
+
+
+/*PROTOTYPES*/
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+void swap_elems(int *elem1, int *elem2);
+void bubble_sort(int *array, size_t size);
+
+
+#endif
