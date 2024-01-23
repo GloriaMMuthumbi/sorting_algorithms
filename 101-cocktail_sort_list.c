9658d0cfef81137bc40327e2fb6ef8dd8b3fffb5
GloriaMMuthumbi <muthumbigloria.m@gmail.com>
2024-01-23 11:41:09 -0800

    5. Cocktail shaker sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,5 +1,5 @@
 #include "sort.h"
-/***
+/**
  * cocktail_sort_list - sorts a doubly linked list of integers in
  * ascending order using the cocktail shaker sort algorithm. prints
  * the list after every swap.
